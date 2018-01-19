43123e40c73724fafc7811f81b35b92065f4b6fe
Arpan <Arpan@Arpan-PC>
2018-01-19 12:12:11 +0530

    Theme applied project

---
+++ ThemeProject/src/main/java/com/ps/test/controller/ThemeContoller.java
@@ -0,0 +1,158 @@
+package com.ps.test.controller;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import com.ps.test.dto.RegisterDTO;
+import com.ps.test.service.login.RegisterService;
+
+
+@Controller
+public class ThemeContoller {
+	
+	@Autowired
+	private RegisterService service;
+	
+	@Autowired
+	HttpSession session= null;
+	
+/*	@Autowired
+	JasperPdfGeneration pdf;
+	*/
+	
+	@RequestMapping(value="/test")
+	public ModelAndView homePage(){
+		System.out.println("inside the controller");
+		return new ModelAndView("home");
+	}
+	
+
+	@RequestMapping("/login")
+	public ModelAndView login(HttpServletRequest request){
+		return new ModelAndView("login");
+		
+	}
+	
+	@RequestMapping("/register")
+	public ModelAndView registerUser(){
+		return new ModelAndView("register");
+	}
+
+	
+	@RequestMapping(value="/saveregister")
+	public String saveUser(@ModelAttribute RegisterDTO dto){
+		System.out.println("inside save register");
+		System.out.println("name-->"+dto.getFname());
+		System.out.println("--->"+(dto.getPassword().equals(dto.getConfirmpwd())));
+		if(!(dto.getPassword().equals(dto.getConfirmpwd())))
+		{
+			
+			return "redirect:/register.html";
+		}
+		
+		else{
+			service.saveRegistration(dto);	
+		}
+		
+		return "redirect:/login.html"; 
+		 
+		
+	}
+	
+	@RequestMapping("/loginapp")
+	public String logon(@RequestParam(value="email1") String email,@RequestParam(value="pwd") String password){
+		System.out.println("inside loginapp");
+		System.out.println("val of-->"+email);
+		int i=0;
+		try{
+			
+	 i = service.checklogin(email,password);
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	
+	if(i>0){
+		return "redirect:/test.html";
+	}
+	else{
+		return "login";
+	}
+}
+	
+	
+	@RequestMapping("/forgotpassword")
+	public ModelAndView forgotpass(){
+		return new ModelAndView("forgotpassword");
+	}
+	
+	
+	@RequestMapping(value="/resetpwd", method=RequestMethod.POST)
+	public String resetpwd(@RequestParam(name="email1") String email,@RequestParam(name="pwd") String pwd,@RequestParam(name="pwd2") String repwd){
+		
+		System.out.println("inside the reset");
+		
+		String emailcheck=service.checkEmail(email);
+		
+		System.out.println("val--->"+( !(pwd.equals(repwd)) ||  emailcheck != null ));
+		
+		if(!( !(pwd.equals(repwd)) ||  emailcheck != null ))
+		{
+			return "redirect:/forgotpassword.html";
+		}
+		else{
+			
+		service.updatedetails(email,pwd,repwd);
+		}
+		return  "redirect:/login.html";
+		
+	}
+	
+	/*@RequestMapping("/pdfgen")
+	public void downloadpdf(HttpServletResponse resp) throws JRException, IOException,BeansException{
+		
+		List<RegisterDTO> list =service.getAllData();
+		for (RegisterDTO registerDTO : list) {
+			System.out.println("value-->"+registerDTO.getFname());
+		}
+		
+		pdf.DownloadPdfReportSingle("jasperReport", list, resp, "jasperReport");
+		
+	}*/
+	
+
+	/* @RequestMapping(method = RequestMethod.GET , value = "pdfgen")
+	    public ModelAndView generatePdfReport(ModelAndView modelAndView){
+	 
+	 
+	        Map<String,Object> parameterMap = new HashMap<String,Object>();
+	 
+	        List<RegisterDTO> list =service.getAllData();
+	 
+	        JRDataSource JRdataSource = new JRBeanCollectionDataSource(list);
+	 
+	        parameterMap.put("datasource", JRdataSource);
+	 
+	        //pdfReport bean has ben declared in the jasper-views.xml file
+	        modelAndView = new ModelAndView("home", parameterMap);
+	 
+	        return modelAndView;
+	 
+	    }//generatePdfReport
+	 
+	*/
+	
+	
+}
---
+++ ThemeProject/src/main/webapp/WEB-INF/jsp/home.jsp
@@ -0,0 +1,50 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+
+<!-- Custom Theme Style -->
+<link href="${pageContext.request.contextPath}/css/custom.min.css"
+	rel="stylesheet">
+</head>
+
+<body>
+
+<div class="nav-md">
+<div class="clearfix" style="min-height: 930px;">
+
+			<div class="right_col" role="main" id="mainCont">
+			<div class="clearfix"></div>
+			<div class="x_panel">
+			<div class="x_title">
+			
+			<h1 align="center">hiiii welcome to the maven project </h1>
+			
+		<div class="clearfix"></div>
+			</div>
+			
+				<div class="x_content">
+			<form action="">			
+			
+			<h6>gghjhghghghg</h6>
+			<h6>asfagsfahgsfahfsdha</h6>
+			
+			<a href="pdfgen.html"><input type="button" value="Pdf Generation"></a>
+			</form>
+			
+			</div>
+			</div>
+			</div>
+			</div>
+</div>
+
+<!-- <div >
+<h1>hiiii welcome to the maven project </h1>
+
+</div>
+<h1>hiiii welcome to the maven project </h1>
+</body> -->
+</html>
